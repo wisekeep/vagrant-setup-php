a3733a831a8e0c33ee047f9bdd4a8a646a119ed2
Marcelo Pires de Almeida <m4rc3lo.almeida@gmail.com>
2019-04-08 09:56:04 -0300

    Alteraçoes para PHP 7.3 + Adicionais

--- Vagrantfile
+++ Vagrantfile
@@ -1,13 +1,18 @@
 Vagrant.configure("2") do |config|
-    config.vm.box = "ubuntu-server-14.04"
-  config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
+	config.vm.box = "ubuntu/trusty64"
     config.vm.network :forwarded_port, guest: 80, host: 8080
     config.vm.network :private_network, ip: "192.168.33.10"
     config.vm.synced_folder "www/", "/var/www", owner: "www-data", group: "www-data", mount_options: ['dmode=777','fmode=666']
     config.vm.synced_folder "~", "/vagrant", owner: "vagrant", group: "vagrant"
     config.vm.provider "virtualbox" do |machine|
     	machine.memory = 1024
-    	machine.name = "ubuntu-server-php"
+    	machine.name = "ubuntu-server-vagrant"
+		machine.customize  ["modifyvm", :id, "--cpus", "1" ]
+		machine.customize  ["modifyvm", :id, "--hwvirtex", "on"]		
+		#machine.customize ["modifyvm", :id, "--vram", "12" ]
+		#machine.customize ["modifyvm", :id, "--accelerate3d", "off"]
+		#machine.customize ["modifyvm", :id, "--accelerate2dvideo", "off"]
     end
+	
     config.vm.provision :shell, path: "setup.sh"
 end
--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "---- Iniciando instalacao do ambiente de Desenvolvimento PHP [EspecializaTI] ---"
+echo "--- Iniciando instalacao do ambiente de Desenvolvimento PHP ---"
 
 echo "--- Atualizando lista de pacotes ---"
 sudo apt-get update
@@ -25,7 +25,7 @@ phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
 EOF
 
 echo "--- Instalando pacotes basicos ---"
-sudo apt-get install software-properties-common vim curl python-software-properties git-core --assume-yes --force-yes
+sudo apt-get install software-properties-common vim curl python-software-properties git-core git-flow --assume-yes --force-yes
 
 echo "--- Adicionando repositorio do pacote PHP ---"
 sudo add-apt-repository ppa:ondrej/php
@@ -34,15 +34,25 @@ echo "--- Atualizando lista de pacotes ---"
 sudo apt-get update
 
 echo "--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
-sudo apt-get install mysql-server-5.5 mysql-client phpmyadmin --assume-yes --force-yes
+sudo apt-get install mysql-server mysql-client sqlite3 libsqlite3-dev libsqlite3-doc phpmyadmin --assume-yes --force-yes
 
 echo "--- Instalando PHP, Apache e alguns modulos ---"
-sudo apt-get install php7.1 php7.1-common --assume-yes --force-yes
-sudo apt-get install php7.1-cli libapache2-mod-php7.1 php7.1-mysql php7.1-curl php-memcached php7.1-dev php7.1-mcrypt php7.1-sqlite3 php7.1-mbstring php*-mysql  php-gd php-xml php-mbstring  zip unzip --assume-yes --force-yes
+sudo apt-get install php7.3 php7.3-cli php7.3-common --assume-yes --force-yes
+sudo apt-get install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl --assume-yes --force-yes
 
-echo "--- Habilitando o PHP 7.1 ---"
+echo "--- Instalando PHP 7.3 ---"
+
+sudo apt-get install php7.1-mcrypt php-mbstring php7.3-mbstring php7.3-sqlite3 php7.3-dev php-gd php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml php7.3-fpm libapache2-mod-php7.3 php7.3-imagick php7.3-recode php7.3-tidy php7.3-xmlrpc php7.3-intl php-memcached --assume-yes --force-yes
+sudo apt-get install php*-mysql php-xml zip unzip  --assume-yes --force-yes
+sudo apt-get install 7.3-* --assume-yes --force-yes
+
+echo "--- Habilitando o PHP 7.3 ---"
 sudo a2dismod php5
-sudo a2enmod php7.1
+sudo a2dismod 7.0
+sudo a2dismod 7.1
+sudo a2dismod 7.2
+
+sudo a2enmod php7.3
 
 echo "--- Habilitando mod-rewrite do Apache ---"
 sudo a2enmod rewrite
@@ -56,8 +66,26 @@ sudo mv composer.phar /usr/local/bin/composer
 
 echo "--- Instalando Banco NoSQL -> Redis <- ---" 
 sudo apt-get install redis-server --assume-yes
-sudo apt-get install php7.1-redis --assume-yes
+sudo apt-get install php7.3-redis --assume-yes
 
 # Instale apartir daqui o que você desejar 
+
+sudo apt install swapspace --assume-yes
+sudo apt install memcached --assume-yes
+sudo service apache2 restart
+sudo apt-get update && sudo apt-get upgrade --assume-yes
+sudo apt-get install rng-tools --assume-yes
+sudo apt-get autoremove --assume-yes
+
+# Webmin dependencies
+sudo apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python --assume-yes
+
+sudo wget http://www.webmin.com/jcameron-key.asc
+sudo apt-key add jcameron-key.asc
+echo "deb http://download.webmin.com/download/repository sarge contrib" | sudo tee -a /etc/apt/sources.list
+sudo apt-get update
+sudo apt install webmin --assume-yes
+sudo apt-get install sqlitebrowser --assume-yes
+sudo apt install npm --assume-yes
 
 echo "[OK] --- Ambiente de desenvolvimento concluido ---"
